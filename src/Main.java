34ce18ee51fb0f298a488b5db4a45dd98813d32f
VladimirZasimovich <vladimirzasimovic@gmail.com>
2019-04-04 20:57:30 +0300

    Исправил цикл while для класса Main

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String choseCalc;
+
         Calculator arabicCalculator = new Calculator();
         Calculator romanCalculator = new Calculator();
         do {
             System.out.println("Chose calculator: ARABIC or ROM and enter it to the console");
             Scanner sc = new Scanner(System.in);
             choseCalc = sc.nextLine();
-            
+
             System.out.println("Chose your operation and enter it to the console:");
             System.out.print("add - addition; ");
             System.out.print("sub - subtraction; ");
@@ -24,11 +25,8 @@ public class Main {
                 romanCalculator.enteredArithmeticActionRoman();
             } else
                 break;
-
-        } while (choseCalc.equalsIgnoreCase("arabic") || choseCalc.equalsIgnoreCase("rom") ||
-                arabicCalculator.getOperation().equalsIgnoreCase("add") || arabicCalculator.getOperation().equalsIgnoreCase("sub") ||
-                arabicCalculator.getOperation().equalsIgnoreCase("mult") || arabicCalculator.getOperation().equalsIgnoreCase("div") ||
-                romanCalculator.getOperation().equalsIgnoreCase("add") || romanCalculator.getOperation().equalsIgnoreCase("sub") ||
-                romanCalculator.getOperation().equalsIgnoreCase("mult") || romanCalculator.getOperation().equalsIgnoreCase("div"));
+        } while (!choseCalc.equalsIgnoreCase("ex")
+                || !arabicCalculator.getOperation().equalsIgnoreCase("ex")
+                || !arabicCalculator.getOperation().equalsIgnoreCase("ex"));
     }
 }
